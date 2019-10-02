80491b79f398da778af6866c3293b07fc6033d18
Maksim Kryshneu <maks.kryshnev@gmail.com>
2019-10-02 22:50:36 +0300

    1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.trunc((this.setRange())*0.5)
     }
 
     lower() {
-
+        return this.min=Math.trunc((this.setRange())*0.5);
     }
 
     greater() {
-
+        return this.max=Math.trunc((this.setRange())*0.5);
     }
 }
 
